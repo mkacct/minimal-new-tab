3782340208499660bfe8ac5095b4e80319e9482b
Madeline Kahn <mk-acct@outlook.com>
2021-07-24 14:37:12 -0400

    unremove necessary favicon switching code

--- js/newtab.js
+++ js/newtab.js
@@ -3,6 +3,24 @@
 document.addEventListener("contextmenu", function(e) {
 	e.preventDefault();
 });
+
 document.addEventListener("wheel", function(e) {
 	if (e.ctrlKey) {e.preventDefault();}
 }, {passive: false});
+
+window.matchMedia("(prefers-color-scheme: dark)").addListener(function(e) {
+	setFavicon(e.matches);
+});
+
+setFavicon(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+function setFavicon(isDark) {
+	document.head.removeChild(document.getElementById("favicon"));
+	var favicon = document.createElement("link");
+	favicon.rel = "icon";
+	favicon.type = "image/png";
+	favicon.sizes = "32x32";
+	favicon.href = "res/" + (isDark ? "favicon-dark.png" : "favicon.png");
+	favicon.id = "favicon";
+	document.head.appendChild(favicon);
+}
